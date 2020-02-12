e86231cf6ccffcd3ff0be3fa451b9d40ba633daf
Rutfebrianti <rut.febrianty@ti.ukdw.ac.id>
2020-02-12 13:02:01 +0700

    Initial commit

---
+++ app/src/main/java/com/example/progandro/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.progandro;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) { //menyimpan instance
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
