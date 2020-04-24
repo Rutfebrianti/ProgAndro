2092d8ef4963fadff6204ebf3e69da5f102492e5
Rutfebrianti <rut.febrianty@ti.ukdw.ac.id>
2020-04-24 20:03:23 +0700

    Add camera

--- app/src/main/java/com/example/progandro/Login.java
+++ app/src/main/java/com/example/progandro/Login.java
@@ -3,51 +3,90 @@ package com.example.progandro;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class Login extends AppCompatActivity {
 
     SharedPreferences sharedPreferences;
     public static final String LOGIN_PREF = "Login";
     public static final String REMEMBER_PREF = "GetIn";
+    private FirebaseAuth firebaseAuth;
+    FirebaseUser firebaseUser;
+
+    private static final String TAG = "login";
 
     DatabaseHelper databaseHelper;
 
     private EditText email;
     private EditText password;
 
-    String emailMasuk;
-    String passwordMasuk;
-
     Button buttonLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) { //menyimpan instance
-
-        sharedPreferences = getSharedPreferences(LOGIN_PREF, MODE_PRIVATE);
-        String check = sharedPreferences.getString(REMEMBER_PREF, "");
-
-        if (check.equals("true")) {
-            startActivity(new Intent(Login.this, HomePageActivity.class));
-            finish();
-        }
-        databaseHelper = new DatabaseHelper(this);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        firebaseUser = firebaseAuth.getCurrentUser();
 
         email = findViewById(R.id.email);
         password = findViewById(R.id.password);
         buttonLogin = findViewById(R.id.login);
         TextView daftar = findViewById(R.id.daftar);
 
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final String emailInput = email.getText().toString();
+                final String passwordInput = password.getText().toString();
 
+                //jika field kosong
+                if (emailInput.isEmpty()) {
+                    email.setError("Please Enter Your Email");
+                    email.requestFocus();
+                } else if (passwordInput.isEmpty()) {
+                    password.setError("Please Enter Your Password");
+                    password.requestFocus();
+                } else {
+                    firebaseAuth.signInWithEmailAndPassword(emailInput, passwordInput).addOnCompleteListener(Login.this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                email.getText().clear();
+                                if (firebaseAuth.getCurrentUser().isEmailVerified()) {
+                                    Log.d(TAG, "Login Success");
+                                    Intent mainActivity = new Intent(Login.this, HomePageActivity.class);
+                                    finish();
+                                    startActivity(mainActivity);
+                                } else {
+                                    Toast.makeText(Login.this, "Please Verify Your Account", Toast.LENGTH_SHORT).show();
+                                    password.getText().clear();
+                                }
+                            } else {
+                                Toast.makeText(Login.this, "Incorrect Login Details", Toast.LENGTH_SHORT).show();
+                                email.getText().clear();
+                                password.getText().clear();
+                            }
+                        }
+                    });
+                }
+            }
+        });
 
         daftar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -56,28 +95,5 @@ public class Login extends AppCompatActivity {
             }
         });
 
-        buttonLogin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                emailMasuk = email.getText().toString();
-                passwordMasuk = password.getText().toString();
-                Boolean checkEmailPass = databaseHelper.emailPassword(emailMasuk, passwordMasuk);
-                if (checkEmailPass==true){
-                    Toast.makeText(getApplicationContext(), "Login Succesfully", Toast.LENGTH_SHORT).show();
-
-                    SharedPreferences preferences = getSharedPreferences(LOGIN_PREF, MODE_PRIVATE);
-                    SharedPreferences.Editor editor = preferences.edit();
-                    editor.putString(REMEMBER_PREF, "true");
-                    editor.apply();
-
-                    startActivity(new Intent(Login.this, HomePageActivity.class));
-                    finish();
-                }
-                else{
-                    Toast.makeText(getApplicationContext(),"Login Failed", Toast.LENGTH_SHORT).show();
-                }
-            }
-        });
     }
-
 }
--- app/src/main/java/com/example/progandro/ViewAdapter.java
+++ app/src/main/java/com/example/progandro/ViewAdapter.java
@@ -13,6 +13,7 @@ public class ViewAdapter extends FragmentPagerAdapter {
         activities = new Fragment[]{
                 new Homepage(),
                 new About(),
+                new TestCamera(),
         };
     }
 
